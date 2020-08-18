b8b691278893e931d06c1d452d6df10f6b48d1e9
sujaykundu777 <sujaykundu777@gmail.com>
2020-08-19 00:10:12 +0530

    dropdown signs mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,38 @@ window.onclick = function(event) {
         event.target.style.display = "none";
     }
 }
+
+// initializing count as 0
+let count = 0;
+
+// when menu is clicked in mobile
+ function dropMenu(){
+    count = count + 1;
+    if(count % 2 === 0){
+     document.querySelector('.icon').innerHTML = "&#9776;";
+    }else{
+      document.querySelector('.icon').innerHTML = "&#9932;";
+    }
+ }
+
+ // main menu
+let mainMenu = document.getElementById('menu').addEventListener("click", dropMenu);
+
+ let dropDownClick = {
+    handleEvent(event) {
+      if(event.currentTarget.classList.contains('dropdown-menu')){
+        if(event.currentTarget.querySelector('span').innerHTML === '+'){
+          event.currentTarget.querySelector('span').innerHTML = '-'
+        }else{
+          event.currentTarget.querySelector('span').innerHTML = '+'
+        }
+      }
+    }
+  };
+
+// dropdown menu
+let dropDownMenu = document.querySelectorAll('.dropdown-menu');
+for(let i=0; i<dropDownMenu.length; i++){
+  dropDownMenu[i].addEventListener("click",dropDownClick);
+}
+
